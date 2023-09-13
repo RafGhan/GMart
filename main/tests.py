6c61d0c96959c3f3f7aad65616177d37e1026c4b
RafGhan <124992862+RafGhan@users.noreply.github.com>
2023-09-13 11:39:11 +0700

    Update tests.py

--- main/tests.py
+++ main/tests.py
@@ -10,3 +10,7 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_templates_contain_title(self):
+        response = Client().get('/main/')
+        self.assertContains(response, '<h1>GMart Page</h1>')
