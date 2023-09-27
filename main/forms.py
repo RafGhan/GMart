8421f7ca8a45ab56afef7bfa4715aac154ab7552
RafGhan <rgharditama@gmail.com>
2023-09-27 11:27:07 +0700

    Update Tugas 4

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,11 @@
 from django.forms import ModelForm
 from main.models import Item
+from django import forms   # NEW!
+from django.contrib.auth.models import User   
 
 class ItemForm(ModelForm):
     class Meta:
         model = Item
         fields = ["name", "amount", "description", "type"]
+    
+    user = forms.ModelChoiceField(queryset=User.objects.all(), required=True)  # NEW
